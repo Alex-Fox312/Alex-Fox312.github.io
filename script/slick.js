e7238c8a9f0a238fa09807a67cc4ce1cafde07c4
Alex-Fox312 <76611441+Alex-Fox312@users.noreply.github.com>
2021-06-05 00:16:45 +0300

    Add files via upload

---
+++ script/slick.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: $('.arrow_right'),
+        prevArrow: $('.arrow_left'),
+    });
+
+    $('.autoplay_2').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2100,
+        nextArrow: $('.arrow_right2'),
+        prevArrow: $('.arrow_left2'),
+    });
+    
+    $('.autoplay_3').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2200,
+        nextArrow: $('.arrow_right3'),
+        prevArrow: $('.arrow_left3'),
+    });
+
+    $('.autoplay_4').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2300,
+        nextArrow: $('.arrow_right4'),
+        prevArrow: $('.arrow_left4'),
+    });
+
+    $('.autoplay_5').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: $('.arrow_right5'),
+        prevArrow: $('.arrow_left6'),
+    });
+  });
+  
+                  
