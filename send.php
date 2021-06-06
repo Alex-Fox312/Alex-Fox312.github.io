3a9e8e9d9a03abcf0b6a680ca0b89831f23c972f
Alex-Fox312 <76611441+Alex-Fox312@users.noreply.github.com>
2021-06-06 16:24:43 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,17 +1,26 @@
 <?php
-$fio = $_POST['fio'];
-$number = $_POST['number'];
-$fio = htmlspecialchars($fio);
-$number = htmlspecialchars($number);
-$fio = urldecode($fio);
-$number = urldecode($number);
-$fio = trim($fio);
-$number = trim($number);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("islam747p@gmail.com", "Заявка с сайта", "ФИО:".$fio.". Number: ".$number ,"From: test@mail.ru \r\n"))
- {     echo "сообщение успешно отправлено";
+//проверяем, существуют ли переменные в массиве POST
+if(!isset($_POST['fio']) and !isset($_POST['email'])){
+ ?> <form action="send.php" method="post">
+<input type="text" name="fio" placeholder="Укажите ФИО" required>
+<input type="text" name="number" placeholder="Укажите number" required>
+<input type="submit" value="Отправить">
+</form> <?php
 } else {
-    echo "при отправке сообщения возникли ошибки";
-}?>
+ //показываем форму
+ $fio = $_POST['fio'];
+ $number = $_POST['number'];
+ $fio = htmlspecialchars($fio);
+ $number = htmlspecialchars($number);
+ $fio = urldecode($fio);
+ $number = urldecode($number);
+ $fio = trim($fio);
+ $number = trim($number);
+ //echo $fio;
+ //echo "<br>";
+ //echo $email;
+ if (mail("islam747p@gmail.com", "Заявка с сайта", "ФИО:".$fio.". Number: ".$number ,"From: test@mail.ru \r\n"))
+  {     echo "сообщение успешно отправлено";
+ } else {
+     echo "при отправке сообщения возникли ошибки";
+ }?>
