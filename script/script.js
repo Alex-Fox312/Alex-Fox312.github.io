dd29521ff975f2ac57e71bd94493e11e903a825a
Alex-Fox312 <76611441+Alex-Fox312@users.noreply.github.com>
2021-06-13 15:43:49 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -133,3 +133,12 @@ window.onclick = function(event) {
     modal5.style.display = "none";
   }
 }
+
+   (function (d, w, c) {
+       (w[c] = w[c] || []).push({formId:158462,host:"formdesigner.ru",formHeight:100, el: "form_158462_1",center: 1,scroll: 0});
+       var s = d.createElement("script"), g = "getElementsByTagName";
+       s.type = "text/javascript"; s.charset="UTF-8"; s.async = true;
+       s.src = (d.location.protocol == "https:" ? "https:" : "http:")+"//formdesigner.ru/js/iform.js?v=0.0.2";
+       var h=d[g]("head")[0] || d[g]("body")[0];
+       h.appendChild(s);
+   })(document, window, "fdforms");
