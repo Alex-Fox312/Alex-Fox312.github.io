0cd1a52213f7339c92a8413b8c23e84b01801b54
Alex-Fox312 <76611441+Alex-Fox312@users.noreply.github.com>
2021-06-17 22:03:06 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -52,6 +52,7 @@ window.onclick = function(event) {
   }
 }
 
+
 // Получить модальный
 var modal3 = document.getElementById("myModal3");
 
@@ -134,11 +135,3 @@ window.onclick = function(event) {
   }
 }
 
-   (function (d, w, c) {
-       (w[c] = w[c] || []).push({formId:158462,host:"formdesigner.ru",formHeight:100, el: "form_158462_1",center: 1,scroll: 0});
-       var s = d.createElement("script"), g = "getElementsByTagName";
-       s.type = "text/javascript"; s.charset="UTF-8"; s.async = true;
-       s.src = (d.location.protocol == "https:" ? "https:" : "http:")+"//formdesigner.ru/js/iform.js?v=0.0.2";
-       var h=d[g]("head")[0] || d[g]("body")[0];
-       h.appendChild(s);
-   })(document, window, "fdforms");
